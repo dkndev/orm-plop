5f960e0144a0efa8c671b032592cde7ecb00aa5b
ldiebold <luke@ldiebold.com>
2020-05-23 12:38:50 +1000

    remove tests dir. Only use it for local dev

--- tests/plopfile.js
+++
@@ -1,8 +0,0 @@
-let core = require('../core')
-let newModelGenerator = require('../newModelGenerator')
-
-module.exports = function (plop) {
-  core(plop);
-
-  plop.setGenerator('model', newModelGenerator('./api', './app', './admin'));
-};
